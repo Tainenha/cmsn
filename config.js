173d601e3fb83fbcc02bb7b4d41759e61cca0c4a
Tainenha <151901277+Tainenha@users.noreply.github.com>
2023-11-24 18:02:52 +0700

    Add files via upload
    cmsn

---
+++ config.js
@@ -0,0 +1,13 @@
+const nameGirl = 'Lê Hồ An Nhiên';
+const giftUrl = 'https://www.facebook.com/profile.php?id=100073863767043';
+const eventName = 'Happy Birthday';
+const titleCard = 'Tặng Bạn Trẻ Lê Hồ An Nhiên';
+const contentCard = 'Chúc Nhiên của anh sinh nhật vui có thật là nhiều sức khoẻ tuổi lớn nhiều thành công lớn hơn nữa và sớm đạt được mong ước của mình nhé. Mãi mãi một tình yêu hướng đạo.(Bành Thị Thoại)\n Chúc bé Nhiên sinh nhật vui vẻ. Chúc em có thật nhiều sức khỏe, đạt được nguyện vọng trong tuổi mới của mình. Vững bước trên con đường Hướng Đạo, cố gắng hoàn thiện bản thân để nắm đội và phát huy vai trò đội trưởng mình nhé.(Thầy Anh Tứng) ';
+
+// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+// Cần hỗ trợ hãy liên hệ: 
+// Mr-Nam http://facebook.com/nam.nodemy
+// Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
+const giftImage = 'hot-girl.png';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
